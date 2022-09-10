eb9deac37eec0b1614e87931e50014b954836654
Adelson Castillo <adelsonramon@gmail.com>
2022-09-10 16:06:14 -0500

    updated nesting

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,74 @@
 <script setup>
-import {ref} from 'vue'
-import { useStorage } from '@vueuse/core'
-import {nanoid} from 'nanoid'
-import canvasConfetti from 'https://cdn.skypack.dev/canvas-confetti';
+  import { ref } from 'vue'
+  import { useStorage } from '@vueuse/core'
+  import { nanoid } from 'nanoid'
+  import confetti from 'canvas-confetti'
 
-const newGrocery = ref('')
-const groceries = useStorage('groceries', [])
+  const newGrocery = ref('')
+  const groceries = useStorage('groceries', [])
+
   const addGrocery = () => {
-    if(newGrocery.value) {
-      groceries.value.push({ id: nanoid(), name: newGrocery.value})
+    if (newGrocery.value) {
+      groceries.value.push({ id: nanoid(), name: newGrocery.value })
       newGrocery.value = ''
     }
   }
 
   const deleteGrocery = id => {
-    const removeIndex  = groceries.value. findIndex(grocery => grocery.id == id)
+    const removeIndex = groceries.value.findIndex(grocery => grocery.id === id)
     groceries.value.splice(removeIndex, 1)
-    canvasConfetti({ particleCount: 300, spread: 1000, origin: {y: 0 } })
+    confetti({ particleCount: 300, spread: 1000, origin: { y: 1 } })
   }
 </script>
 
 <template>
-  <Main>
-  <h1 class="title">📝 Vue Grocery List 📝</h1>
-  <form class="newGroceryForm" @submit.prevent="addGrocery">
-    <input 
-    id="newGrocery"
-    autocomplete="off" 
-    type="text" 
-    placeholder="Add an item to your list"
-    v-model="newGrocery"
-    />
-    <button type="submit">Add</button>
-  </form>
-  <h3>Pending Items: {{ groceries.length }}</h3>
-  <ul id="groceryList">
-    <li v-for="grocery in groceries" @click="deleteGrocery(grocery.id)">
-      {{ grocery.name }}</li>
-    </ul>cer
-  </Main>
+  <main>
+    <h1 class="title">📝 Vue Grocery List 📝</h1>
+    <form class="newGroceryForm" @submit.prevent="addGrocery">
+      <input
+        id="newGrocery"
+        autocomplete="off"
+        type="text"
+        placeholder="Add an item to your list"
+        v-model="newGrocery"
+      />
+      <button type="submit">Add</button>
+    </form>
+    <h3>Pending Items: {{ groceries.length }}</h3>
+    <ul>
+      <li v-for="grocery in groceries" @click="deleteGrocery(grocery.id)">
+        {{ grocery.name }}
+      </li>
+    </ul>
+  </main>
 </template>
 
 <style lang="postcss" scoped>
   main {
-    @apply flex flex-col justify-center items-center gap-8;
-  }
-  .title{
-    @apply m-2 text-6xl font-light tracking-wider text-accent;
-  }
-  form {
-    @apply mt-8 flex focus-within:ring-8 focus-within:ring-accent focus-within:rounded-lg;
-    input {
-      @apply bg-slate-300 text-comment p-2 w-80 text-2xl rounded-l-md;
-      &:hover {
-        @apply bg-purplish;
+    @apply mt-8 flex flex-col justify-center items-center gap-8;
+    .title {
+      @apply m-2 text-6xl font-light tracking-wider text-accent;
+    }
+    form {
+      @apply flex focus-within:ring-8 focus-within:ring-accent focus-within:rounded-lg;
+      input {
+        @apply bg-white text-comment p-2 w-80 text-2xl rounded-l-md outline-none;
+      }
+      button {
+        @apply bg-accent text-background p-2 text-2xl font-bold rounded-r-md;
+        &:hover {
+          @apply bg-purplish;
+        }
       }
     }
-    button {
-      @apply bg-accent text-background p-2 text-2xl font-bold  rounded-r-md; 
+    ul {
+      @apply flex flex-col items-center justify-center rounded-lg bg-comment;
+      li {
+        @apply bg-white text-background m-2 p-2 w-96 text-center;
+        &:hover {
+          @apply bg-purplish font-bold cursor-pointer;
+        }
+      }
     }
   }
-ul {
-  @apply flex flex-col items-center justify-center rounded-lg bg-comment;
-  li{
-    @apply bg-white text-background m-2 p-2 w-96 text-center;
-    &:hover {
-      @apply bg-purplish font-bold cursor-pointer;
-    }
-  }
-}
-   
 </style>
